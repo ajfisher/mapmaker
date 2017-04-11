f399682a39546861837acf273314d608902f6666
Andrew Fisher <ajfisher.td@gmail.com>
2017-04-11 17:32:53 +1000

    Maintenance update trying to get links across geometry

--- index.js
+++ index.js
@@ -36,8 +36,79 @@ g.delaunay line {
 }
 `;
 
+var diagram, polygons;
+var polys, poly_links;
 var sites = [];
+var poly_queue = []; // used to hold queue of polygons for processing
 var color = d3.scaleSequential(d3.interpolateViridis);
+
+function create_island() {
+
+    // creates an island at a random point.
+    let startpoly = diagram.find(
+        Math.random() * width,
+        Math.random() * height
+    ).index;
+
+    let highpoint = (Math.random() * 0.5) + 0.5; // bind between 0.5 and 1
+    let radius = (Math.random() * 0.1) + 0.899; // bind between 0.9 and 0.999
+    let sharpness = (Math.random() * 0.5); // bind between 0 - 0.5
+
+    console.log(startpoly, highpoint, radius, sharpness);
+    polygons[startpoly].height = highpoint;
+    polygons[startpoly].used = true;
+    poly_queue.push(startpoly);
+
+    // put heights around the island values
+    for (let i = 0; i < poly_queue.length && highpoint > 0.01; i++) {
+
+        highpoint = highpoint * radius;
+
+        // get each of the neighbours of the start poly and iterate overthem
+        diagram.cells[ poly_queue[i] ].halfedges.forEach( (e) => {
+            console.log(e);
+            if (typeof(polygons[e]) === 'undefined') {
+                return;
+            } else if (! polygons[e].used) {
+                console.log("setting a height");
+                polygons[e].height = polygons[e].height + height;
+
+                // set a maximum height to 1.0
+                if (polygons[e].height > 1) {polygons[e].height = 1.0;}
+                polygons[e].used = true;
+
+                poly_queue.push(e);
+            }
+        });
+    }
+
+    console.log(diagram.cells[0]);
+    console.log(sites[0])
+    console.log(polygons[0]);
+    console.log(poly_links[0]);
+
+
+    polys.attr('fill', (d, i) => { 
+        if (polygons[i].height) {
+            console.log("height set");
+            return "grey";
+        } else {
+            return "blue";
+        }
+    });
+    // colour the polygons based on height
+
+}
+
+
+function colour_polys() {
+    // sets the colour of the polygons based on height map.
+
+    polygons.map(i => {
+        //console.log(i);
+    });
+}
+
 
 function generate_map() {
     console.log("generating map");
@@ -59,16 +130,21 @@ function generate_map() {
 
     var voronoi = d3.voronoi().extent([ [0, 0], [width, height] ]);
     sites = voronoi(sites).polygons().map(d3.polygonCentroid);
-    var diagram = voronoi(sites);
-    var polygons = diagram.polygons();
+    diagram = voronoi(sites);
+    polygons = diagram.polygons();
+    poly_links = diagram.links();
+
+    // do some initialisation;
+    polygons.forEach( p => { p.used = false; });
 
     // show the cells
-    var polys = svg.append("g")
+    polys = svg.append("g")
         .attr("class", "tessel")
         .selectAll("path")
         .data(polygons)
         .enter()
         .append('path')
+        .attr('id', (d,i) => i)
         .attr('d', d => ("M" + d.join("L") + "Z"))
         .attr('fill', (d, i) => color(i/site_num) );
 
@@ -77,7 +153,7 @@ function generate_map() {
     var links = svg.append("g")
         .attr("class", "delaunay")
         .selectAll("line")
-        .data(voronoi.links(sites))
+        .data(poly_links)
         .enter()
         .append("line")
         .attr('x1', d => d.source[0])
@@ -95,6 +171,9 @@ function generate_map() {
         .attr("cx", d => d[0] )
         .attr("cy", d => d[1] );
 
+    create_island();
+
+    colour_polys();
 }
 
 
